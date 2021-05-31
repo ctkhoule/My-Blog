b9c3b7fe078e30ebc1a1d544d3c4c54cc3858208
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2021-05-31 12:36:23 +0000

    Create Post model, migration and factory

---
+++ app/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'seo_title',
+        'excerpt',
+        'body',
+        'meta_description',
+        'meta_keywords',
+        'active',
+        'image',
+        'user_id',
+    ];
+}
